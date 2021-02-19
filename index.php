1abb294e4a786f46c74ef27b9da60ae640bea8f1
4lorelei <b.sar7o@gmail.com>
2021-02-20 00:32:53 +0100

    no message

--- index.php
+++ index.php
@@ -1,25 +1,42 @@
 <?php
 
-  print ("Hello");
-  
-  // Create an image, 1x1 pixel in size
-  $im=imagecreate(1,1);
+  ignore_user_abort(true);
 
-  // Set the background colour
-  $white=imagecolorallocate($im,255,255,255);
+// turn off gzip compression
+if ( function_exists( 'apache_setenv' ) ) {
+  apache_setenv( 'no-gzip', 1 );
+}
 
-  // Allocate the background colour
-  imagesetpixel($im,1,1,$white);
+ini_set('zlib.output_compression', 0);
 
-  // Set the image type
-  header("content-type:image/jpg");
+// turn on output buffering if necessary
+if (ob_get_level() == 0) {
+  ob_start();
+}
 
-  // Create a JPEG file from the image
-  imagejpeg($im);
+// removing any content encoding like gzip etc.
+header('Content-encoding: none', true);
 
-  // Free memory associated with the image
-  imagedestroy($im);
-  
-  print("World!");
+//check to ses if request is a POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // the GIF should not be POSTed to, so do nothing...
+  echo ' ';
+} else {
+  // return 1x1 pixel transparent gif
+  header("Content-type: image/gif");
+  // needed to avoid cache time on browser side
+  header("Content-Length: 42");
+  header("Cache-Control: private, no-cache, no-cache=Set-Cookie, proxy-revalidate");
+  header("Expires: Wed, 11 Jan 2000 12:59:00 GMT");
+  header("Last-Modified: Wed, 11 Jan 2006 12:59:00 GMT");
+  header("Pragma: no-cache");
+
+  echo sprintf('%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%',71,73,70,56,57,97,1,0,1,0,128,255,0,192,192,192,0,0,0,33,249,4,1,0,0,0,0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59);    
+}
+
+// flush all output buffers. No reason to make the user wait for OWA.
+ob_flush();
+flush();
+ob_end_flush()
 
 ?>
