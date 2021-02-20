1bfa8aee095c529851dd1d5a39ab16543fcc8af2
4lorelei <b.sar7o@gmail.com>
2021-02-20 19:56:26 +0100

    no message

--- img.php
+++ img.php
@@ -10,10 +10,11 @@ print $contents;
 $ip = $_SERVER['REMOTE_ADDR'];
 $referer = $_SERVER['HTTP_REFERER'];
 $useragent = $_SERVER['HTTP_USER_AGENT'];
+$request_uri = $_SERVER['REQUEST_URI'];
 $browser = get_browser(null, true);
 
 $log = file_get_contents("traccia.txt");
 $dd=date("d/m/Y H:i:s");
-$log =  $log . "\n" . $ip . "-" .$referer . "-" . $useragent . "-" . $browser . "-" . $dd;
+$log =  $log . "\n" . $ip . "-" .$referer . "-" . $useragent . "-" . $browser . "-" . $request_uri . "-" . $dd;
 	
 file_put_contents("traccia.txt", $log, LOCK_EX);
