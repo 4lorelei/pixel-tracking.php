ef16df5065c50a13c1ce70733e5e29b9ce0986c9
4lorelei <b.sar7o@gmail.com>
2021-02-20 17:39:56 +0100

    no message

--- 111.php
+++ 111.php
@@ -19,7 +19,7 @@ $browser = get_browser(null, true);
 $log = file_get_contents("traccia.txt");
 #$log = json_decode($myLogJson,true);
 $dd=date("d/m/Y H:i:s");
-$log =  $ip . "-" .$referer . "-" . $useragent . "-" . $browser . "-" . $dd;
+$log =  $log . "\n" . $ip . "-" .$referer . "-" . $useragent . "-" . $browser . "-" . $dd;
 	
 #$myLogJson = json_encode($log);
 file_put_contents("traccia.txt", $log, LOCK_EX);
