c79f9fb660b0df4280b1af635b9495707fe4bc93
4lorelei <b.sar7o@gmail.com>
2021-02-20 17:20:45 +0100

    no message

--- index.php
+++ index.php
@@ -13,11 +13,12 @@ $useragent = $_SERVER['HTTP_USER_AGENT'];
 $browser = get_browser(null, true);
 
 $myLogJson = file_get_contents("traccia.txt");
-$log = json_decode($myLogJson,true);
+#$log = json_decode($myLogJson,true);
 $dd=date("d/m/Y H:i:s");
-$log[$ip . "-" .$referer . "-" . $useragent . "-" . $browser . "-" .$dd]="";
+$log = $log . "\n" . $ip . "-" .$referer . "-" . $useragent . "-" . $browser . "-" .$dd;
 	
-$myLogJson = json_encode($log);
+$myLogJson = $log;
+#$myLogJson = json_encode($log);
 file_put_contents("traccia.txt", $myLogJson, LOCK_EX);
 
 ?>
